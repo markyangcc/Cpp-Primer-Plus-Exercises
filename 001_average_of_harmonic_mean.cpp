fe720330b80cbd26d4d474444c4f47f34ee047a3
markyangcc <guangshengyang01@gmail.com>
2020-10-01 00:47:36 +0800

    Fix: bugs fixed, count values

--- 001_average_of_harmonic_mean.cpp
+++ 001_average_of_harmonic_mean.cpp
@@ -15,13 +15,15 @@ int main()
 
     cin >> x;
     cin >> y;
-    //Bugs ned fix: count
+
     while (x != 0 && y != 0)
     {
         result = HarmonicMean(x, y);
 
-        sum += result;
+        //plus count num after each function call
         count++;
+
+        sum += result;
 
         cin >> x;
         cin >> y;
@@ -29,7 +31,7 @@ int main()
 
     if (x == 0 || y == 0)
         cout << "The latest input values include 0, input exited by default." << '\n';
-
+    cout << "Total read in pairs num are " << count << '\n';
     cout << "The average of these paired harmonic mean is " << sum / count << '\n';
 
     return 0;
