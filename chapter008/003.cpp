3fd5a5e35ae5832632865ee317162b670be39540
markyangcc <guangshengyang01@gmail.com>
2020-10-05 21:38:56 +0800

    add loop input feature to 003.cpp

--- chapter008/003.cpp
+++ chapter008/003.cpp
@@ -6,14 +6,23 @@ void turn_upper(string &str);
 
 int main()
 {
-    string str = "Hello, cpp!";
-    turn_upper(str);
+    string str;
+
+    cout << "Enter a string(q to quit): ";
+    while ((cin >> str) && str != "q")
+    {
+        turn_upper(str);
+        cout << "Another string: ";
+    }
+
     return 0;
 }
 
 void turn_upper(string &str)
 {
+    cout << "Uppercase string(q to quit): ";
     for (int i = 0; i < str.length(); i++)
         cout << (char)toupper(str[i]);
-    cout << endl;
+    cout << endl
+         << endl;
 }
