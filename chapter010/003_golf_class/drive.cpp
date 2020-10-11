ca0b2dec09552005725b48c409e6e04f3effffca
markyangcc <guangshengyang01@gmail.com>
2020-10-11 17:58:54 +0800

    New style

--- chapter010/003_golf_class/drive.cpp
+++ chapter010/003_golf_class/drive.cpp
@@ -3,20 +3,20 @@
 int main()
 {
     {
-        Golf default_player;
+        Golf p1;
         std::cout << "Default player info about Golf Player: " << std::endl;
-        default_player.showgolf();
+        p1.showgolf();
         std::cout << std::endl;
 
-        Golf new_player;
-        new_player.setgolf();
+        Golf p2;
+        p2.setgolf();
         std::cout << "Info about new Golf" << std::endl;
-        new_player.showgolf();
+        p2.showgolf();
         std::cout << std::endl;
 
-        std::cout << "Reset handicap of Golf new_player to 12" << std::endl;
-        new_player.rset_handicap(12);
-        new_player.showgolf();
+        std::cout << "Reset handicap of Golf p2 to 12" << std::endl;
+        p2.rset_handicap(12);
+        p2.showgolf();
         std::cout << std::endl;
     }
     return 0;
