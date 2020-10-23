4b8a247429b8be6544b6b80a38f22f647b6b1402
markyangcc <guangshengyang01@gmail.com>
2020-10-23 21:40:24 +0800

    No new updates

---
+++ chapter013/004/drive.cpp
@@ -0,0 +1,20 @@
+#include "port.cpp"
+#include "vintageport.cpp"
+
+int main()
+{
+    using std::cout;
+    using std::endl;
+
+    Port p1;
+    p1.show();
+    cout << p1;
+
+    cout << endl
+         << endl;
+
+    Port p2 = {}
+
+
+    return 0;
+}
