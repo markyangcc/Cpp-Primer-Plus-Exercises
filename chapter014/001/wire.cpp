d98875199ad5568a46d1c022e7c395a736f680ec
markyangcc <guangshengyang01@gmail.com>
2020-10-27 18:18:06 +0800

    finish folder 001

--- chapter014/001/wire.cpp
+++ chapter014/001/wire.cpp
@@ -1,14 +1,50 @@
 #include "wire.h"
+
 #include <cstring>
+#include <iostream>
 
-Wine::Wine(const char *l, int y, const int yr[], const int bot[])
-{
-    name = l;
-    year = y;
-    info(yr, bot);
+Wine::Wine(const char *l, int y, const int yr[], const int bot[]) {
+  name = l;
+  year = y;
+
+  //   info.first.resize(year);
+  //   info.second.resize(year);
+
+  for (int i = 0; i < year; i++) {
+    info.first[i] = yr[i];
+    info.second[i] = bot[i];
+  }
 }
-Wine::Wine(const char *l, int y) {}
 
-void Wine::GetBottles() {}
-Wine &Wine::Label() {}
-int Wine::sum() {}
+Wine::Wine(const char *l, int y) {
+  name = l;
+  year = y;
+}
+
+void Wine::GetBottles() {
+  using std::cin;
+  using std::cout;
+
+  for (int i = 0; i < year; i++) {
+    cout << "Line[" << i + 1 << "]: ";
+    cout << "Enter the year: ";
+    cin >> info.first[i];
+    cout << "Enter the bottles: ";
+    cin >> info.second[i];
+  }
+}
+
+const string &Wine::Label() const { return name; }
+int Wine::sum() const { return info.second.sum(); }
+
+void Wine::Show() const {
+  using std::cout;
+  using std::endl;
+
+  cout << "Name: " << name << endl;
+  cout << "Year and bottles:";
+
+  for (int i = 0; i < info.first.size(); i++) {
+    cout << info.first[i] << info.second[i] << endl;
+  }
+}
--- chapter014/001/wire.h
+++ chapter014/001/wire.h
@@ -2,30 +2,32 @@
 #define WINE_H_
 
 #include <string>
-#include "mypair.cpp"
+#include <valarray>
 
+using std::pair;
 using std::string;
 
 typedef std::valarray<int> ArrayInt;
-typedef Pair<ArrayInt, ArrayInt> PairArray;
+typedef pair<ArrayInt, ArrayInt> PairArray;
 
-class Wine
-{
-private:
-    string name;
-    int year;
-    PairArray info;
+class Wine {
+ private:
+  string name;
+  int year;
+  PairArray info;
 
-public:
-    Wine();
-    ~Wine();
+ public:
+  Wine(){};
+  ~Wine(){};
 
-    Wine(const char *l, int y, const int yr[], const int bot[]);
-    Wine(const char *l, int y);
+  Wine(const char* l, int y, const int yr[], const int bot[]);
+  Wine(const char* l, int y);
 
-    void GetBottles();
-    Wine &Label();
-    int sum();
+  void Show() const;
+
+  void GetBottles();
+  const string& Label() const;
+  int sum() const;
 };
 
 #endif
