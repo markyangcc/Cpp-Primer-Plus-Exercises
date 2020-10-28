4a22738073a733d3294233260f0f59a9959f4014
markyangcc <guangshengyang01@gmail.com>
2020-10-28 21:19:30 +0800

    change format

--- chapter014/003/drive.cpp
+++ chapter014/003/drive.cpp
@@ -9,28 +9,34 @@
 #include "workermi.cpp"
 const int SIZE = 5;
 
-int main() {
-  using std::cin;
-  using std::cout;
-  using std::endl;
-  using std::strchr;
+int main()
+{
+  {
+    using std::cin;
+    using std::cout;
+    using std::endl;
+    using std::strchr;
 
-  Queuetp<Worker *> q;
-  Worker *pw;
+    Queuetp<Worker *> q;
+    Worker *pw;
 
-  int ct;
-  for (ct = 0; ct < SIZE; ct++) {
-    char choice;
-    cout << "Enter the employee category:\n"
-         << "w: waiter  s: singer  "
-         << "t: singing waiter  q: quit\n";
-    cin >> choice;
-    while (strchr("wstq", choice) == NULL) {
-      cout << "Please enter a w, s, t, or q: ";
+    int ct;
+    for (ct = 0; ct < SIZE; ct++)
+    {
+      char choice;
+      cout << "Enter the employee category:\n"
+           << "w: waiter  s: singer  "
+           << "t: singing waiter  q: quit\n";
       cin >> choice;
-    }
-    if (choice == 'q') break;
-    switch (choice) {
+      while (strchr("wstq", choice) == NULL)
+      {
+        cout << "Please enter a w, s, t, or q: ";
+        cin >> choice;
+      }
+      if (choice == 'q')
+        break;
+      switch (choice)
+      {
       case 'w':
         pw = new Waiter;
         break;
@@ -40,20 +46,22 @@ int main() {
       case 't':
         pw = new SingingWaiter;
         break;
+      }
+      cin.get();
+      pw->Set();
+      q.enqueue(pw);
     }
-    cin.get();
-    pw->Set();
-    q.enqueue(pw);
-  }
 
-  cout << "\nHere is your staff:\n";
-  int i;
-  for (i = 0; i < ct; i++) {
-    cout << endl;
-    q.dequeue(pw);
-    pw->Show();
-    delete pw;
+    cout << "\nHere is your staff:\n";
+    int i;
+    for (i = 0; i < ct; i++)
+    {
+      cout << endl;
+      q.dequeue(pw);
+      pw->Show();
+      delete pw;
+    }
+    cout << "Bye.\n";
   }
-  cout << "Bye.\n";
   return 0;
 }
--- chapter014/003/queuetp.h
+++ chapter014/003/queuetp.h
@@ -4,96 +4,118 @@
 #include <iostream>
 
 template <typename Type>
-class Queuetp {
- private:
-  struct Node {
-    Type data;
-    Node *next;
-  };
+class Queuetp
+{
+private:
+    struct Node
+    {
+        Type data;
+        Node *next;
+    };
 
-  Node *head;
-  Node *tail;
+    // Don't use nested class here, it make code more complex
+    Node *head;
+    Node *tail;
 
- public:
-  Queuetp();
-  Queuetp(const Queuetp<Type> &q);
-  ~Queuetp();
-  bool enqueue(const Type &val);
-  bool dequeue(const Type &val);
-  bool isempty();
-  Type peek() const;
+public:
+    Queuetp();
+    Queuetp(const Queuetp<Type> &q);
+    ~Queuetp();
+    bool enqueue(const Type &val);
+    bool dequeue(const Type &val);
+    bool isempty();
+    Type peek() const;
 };
 
 template <typename Type>
-Queuetp<Type>::Queuetp() {
-  head = nullptr;
-  tail = nullptr;
+Queuetp<Type>::Queuetp()
+{
+    head = nullptr;
+    tail = nullptr;
 }
 template <typename Type>
 
-Queuetp<Type>::Queuetp(const Queuetp<Type> &q) {
-  Node *t = q.tail;
-  while (t != NULL) {
-    enqueue(t->data);
-  }
+Queuetp<Type>::Queuetp(const Queuetp<Type> &q)
+{
+    // Node *t = q.tail;
+    // while (t != NULL) {
+    //   enqueue(t->data);
+    // }
 }
 
 template <typename Type>
-Queuetp<Type>::~Queuetp() {
-  while (head != nullptr) {
-    Node *tempptr = head;
-    head = head->next;
-    head->next = nullptr;
-    delete[] tempptr;
-  }
+Queuetp<Type>::~Queuetp()
+{
+    while (head != nullptr)
+    {
+        Node *tempptr = head;
+        head = head->next;
+        tempptr->next = nullptr;
+        delete[] tempptr;
+    }
 }
 
 template <typename Type>
-bool Queuetp<Type>::enqueue(const Type &val) {
-  Node *node = new Node;
+bool Queuetp<Type>::enqueue(const Type &val)
+{
+    Node *node = new Node;
 
-  if (node == nullptr) {
-    std::cout << "Error, no more memory aviaiable." << std::endl;
-    return false;
-  } else if (isempty()) {
-    node->next = nullptr;
-    node->data = val;
-    head = node;
-  } else {
-    tail->next = node;
-    node->next = nullptr;
-    node->data = val;
-    tail = node;
-  }
+    if (node == nullptr)
+    {
+        std::cout << "Error, no more memory aviaiable." << std::endl;
+        return false;
+    }
+    else if (isempty())
+    {
+        node->next = nullptr;
+        node->data = val;
+        head = tail = node; // caution here,
+                            // only make head = node; is incorrect
+    }
+    else
+    {
+        tail->next = node;
+        node->next = nullptr;
+        node->data = val;
+        tail = node;
+    }
 
-  return true;
-}
-
-template <typename Type>
-bool Queuetp<Type>::dequeue(const Type &val) {
-  if (isempty()) {
-    std::cout << "Error, queue empty. No element to dequeue." << std::endl;
-    return false;
-  } else {
-    Node *tempptr = head;
-    head = head->next;
-    head->next = nullptr;
-    delete[] tempptr;
-  }
-  return true;
-}
-
-template <typename Type>
-bool Queuetp<Type>::isempty() {
-  if (head == nullptr) {
     return true;
-  } else
-    return false;
 }
 
 template <typename Type>
-Type Queuetp<Type>::peek() const {
-  return head->data;
+bool Queuetp<Type>::dequeue(const Type &val)
+{
+    if (isempty())
+    {
+        std::cout << "Error, queue empty. No element to dequeue." << std::endl;
+        return false;
+    }
+    else
+    {
+        Node *tempptr = head;
+        head = head->next;
+        tempptr->next = nullptr;
+        delete[] tempptr;
+    }
+    return true;
+}
+
+template <typename Type>
+bool Queuetp<Type>::isempty()
+{
+    if (head == nullptr)
+    {
+        return true;
+    }
+    else
+        return false;
+}
+
+template <typename Type>
+Type Queuetp<Type>::peek() const
+{
+    return head->data;
 }
 
 #endif
