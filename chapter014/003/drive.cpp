9ef069d9549c6ad315163ec097894de5fd94bdb7
markyangcc <guangshengyang01@gmail.com>
2020-10-29 10:24:48 +0800

    No substantive commit

--- chapter014/003/drive.cpp
+++ chapter014/003/drive.cpp
@@ -1,7 +1,4 @@
-// pe14-3.cpp -- tests the Queuetp class
-// This is exercise 3 of chapter 14 in C++ Primer Plus 5e by Stephen Prata
-// Note: This program is a modified version of listing 14.12
-// compile with workermi.cpp
+
 #include <cstring>
 #include <iostream>
 
@@ -10,8 +7,8 @@
 const int SIZE = 5;
 
 int main()
+
 {
-  {
     using std::cin;
     using std::cout;
     using std::endl;
@@ -23,45 +20,45 @@ int main()
     int ct;
     for (ct = 0; ct < SIZE; ct++)
     {
-      char choice;
-      cout << "Enter the employee category:\n"
-           << "w: waiter  s: singer  "
-           << "t: singing waiter  q: quit\n";
-      cin >> choice;
-      while (strchr("wstq", choice) == NULL)
-      {
-        cout << "Please enter a w, s, t, or q: ";
+        char choice;
+        cout << "Enter the employee category:\n"
+             << "w: waiter  s: singer  "
+             << "t: singing waiter  q: quit\n";
         cin >> choice;
-      }
-      if (choice == 'q')
-        break;
-      switch (choice)
-      {
-      case 'w':
-        pw = new Waiter;
-        break;
-      case 's':
-        pw = new Singer;
-        break;
-      case 't':
-        pw = new SingingWaiter;
-        break;
-      }
-      cin.get();
-      pw->Set();
-      q.enqueue(pw);
+        while (strchr("wstq", choice) == NULL)
+        {
+            cout << "Please enter a w, s, t, or q: ";
+            cin >> choice;
+        }
+        if (choice == 'q')
+            break;
+        switch (choice)
+        {
+        case 'w':
+            pw = new Waiter;
+            break;
+        case 's':
+            pw = new Singer;
+            break;
+        case 't':
+            pw = new SingingWaiter;
+            break;
+        }
+        cin.get();
+        pw->Set();
+        q.enqueue(pw);
     }
 
     cout << "\nHere is your staff:\n";
     int i;
     for (i = 0; i < ct; i++)
     {
-      cout << endl;
-      q.dequeue(pw);
-      pw->Show();
-      delete pw;
+        cout << endl;
+        q.dequeue(pw);
+        pw->Show();
+        delete pw;
     }
     cout << "Bye.\n";
-  }
-  return 0;
+
+    return 0;
 }
--- chapter014/003/workermi.h
+++ chapter014/003/workermi.h
@@ -85,12 +85,16 @@ public:
     // Waiter was listed before Singer regarding MI
     SingingWaiter(const std::string &s, long n, int p = 0, int v = other)
         : Worker(s, n), Waiter(s, n, p), Singer(s, n, v) {}
+
     SingingWaiter(const Worker &wk, int p = 0, int v = other)
         : Worker(wk), Waiter(wk, p), Singer(wk, v) {}
+
     SingingWaiter(const Waiter &wt, int v = other)
         : Worker(wt), Waiter(wt), Singer(wt, v) {}
+
     SingingWaiter(const Singer &wt, int p = 0)
         : Worker(wt), Waiter(wt, p), Singer(wt) {}
+        
     void Set();
     void Show() const;
 };
